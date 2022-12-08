d5007859faed061c39b05467f4024e0c8b13826f
Ceylene Chambers <ceylene.chambers@bison.howard.edu>
2022-12-08 03:18:22 +0000

    Resubmitting Signals Lab

--- signals/signal.c
+++ signals/signal.c
@@ -3,21 +3,34 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+bool trigger = false;
 
 void handler(int signum)
 { //signal handler
   printf("Hello World!\n");
+  trigger = true;
+  alarm(1);
   //exit(1); //exit after printing
 }
 
 int main(int argc, char * argv[])
 {
-  signal(SIGALRM,handler); //register handler to handle SIGALRM
-  do{
+  for (;;){
+    trigger = false;
     alarm(1); //Schedule a SIGALRM for 1 second
-    sleep(1);
-    printf("Turing was right!\n");
-  } while(1); //busy wait for signal to be delivered
+    signal(SIGALRM, handler); //register handler to handle SIGALRM
+    while(trigger == false){
+      printf("Turing was right!\n");
+      }
+  }
+  
+  // do{
+    
+  //   sleep(1);
+    
+  // } while(1); //busy wait for signal to be delivered
 
   return 0; //never reached
 }
--- signals/timer.c
+++ signals/timer.c
@@ -3,15 +3,21 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdbool.h>
 
 
 int num_alarms = 0;
 time_t start_time;
 time_t end_time; 
 time_t total_secs;
+bool trigger = false;
+
 void handler(int signum)
 { //signal handler
   printf("Hello World!\n");
+  trigger = true;
+  num_alarms++;
   //exit(1); //exit after printing
 }
 
@@ -20,20 +26,24 @@ void handler_sigint(int sec){
   total_secs = end_time - start_time;
   printf("\nNumber of alarms: %d \n", num_alarms);
   printf("The program was executing for; %ld seconds \n", total_secs);
-  exit(1);
+  exit(0);
 }
 
 int main(int argc, char * argv[])
 {
   start_time = time(NULL);
-  signal(SIGALRM,handler); //register handler to handle SIGALRM
   signal(SIGINT, handler_sigint);
-  do{
+  signal(SIGINT, handler);
+  for(;;){
+    trigger = false;
+    signal(SIGALRM,handler); //register handler to handle SIGALRM
     alarm(1); //Schedule a SIGALRM for 1 second
-    num_alarms += 1;
-    sleep(1);
-    printf("Turing was right!\n");
-  } while(1); //busy wait for signal to be delivered
+    // num_alarms += 1;
+    // sleep(1);
+    while(trigger == false){
+      printf("Turing was right!\n");
+    }    
+  }
 
   return 0; //never reached
 }
