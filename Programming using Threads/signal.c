35a7c589c8d1b025ae5593e53c43c725e6a05a8e
Ceylene Chambers <ceylene.chambers@bison.howard.edu>
2022-12-13 04:33:08 +0000

    Lab 2 - Signals

---
+++ Programming using Threads/signal.c
@@ -0,0 +1,25 @@
+/* hello_signal.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdbool.h>
+ 
+bool alarm_trigger = false;
+ 
+void handler(int signum){ //signal handler
+ printf("Hello World!\n");
+ alarm_trigger = true;
+ // exit(1); //exit after printing
+}
+ 
+int main(int argc, char * argv[]){
+ for(;;) {
+   alarm_trigger = false;
+   alarm(1); //Schedule a SIGALRM for 1 second
+   signal(SIGALRM, handler); //register handler to handle SIGALRM
+   while(alarm_trigger == false); //busy wait for signal to be delivered
+     printf("Turing was right!\n");
+ }
+ return 0; //never reached
+}
---
+++ Programming using Threads/timer.c
@@ -0,0 +1,42 @@
+/* timer.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdbool.h>
+ 
+bool alarm_trigger = false;
+int start_time;
+int end_time;
+int alarm_count = 0;
+ 
+void time_handler(int sigint){
+ int total_time;
+ end_time = time(NULL);
+ total_time = end_time - start_time;
+ printf("\nTotal number of seconds = %d\nAlarm count = %d\n", total_time, alarm_count);
+ exit(0);
+}
+ 
+void handler(int signum)
+{ //signal handler
+ printf("Hello World!\n");
+ alarm_trigger = true;
+ alarm_count++;
+ // exit(1); //exit after printing
+}
+ 
+int main(int argc, char * argv[])
+{
+ start_time = time(NULL);
+ signal(SIGINT, time_handler);
+ signal(SIGALRM, handler); //register handler to handle SIGALRM
+ for(;;) {
+   alarm_trigger = false;
+   alarm(3); //Schedule a SIGALRM for 1 second
+   while(alarm_trigger == false); //busy wait for signal to be delivered
+     printf("Turing was right!\n");
+ }
+ return 0; //never reached
+}
